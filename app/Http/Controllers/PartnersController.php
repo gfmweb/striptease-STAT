304d8f2c7c80e6a92f8c7f91807d8d941106290a
Anton <banderazz@inbox.ru>
2020-02-05 18:39:18 +0300

    назначаем партнеру только те каналы, которые не являются группами

--- app/Http/Controllers/PartnersController.php
+++ app/Http/Controllers/PartnersController.php
@@ -91,7 +91,7 @@ class PartnersController extends Controller
 		$userSubProject->save();
 
 		// добавим юзеру все таргеты по каналам с дефолтным комментарием
-		$channels = Channel::all();
+		$channels = Channel::whereNull('parent_id')->get();
 		foreach ($channels as $channel) {
 			$userTarget = new UserTarget;
 			$userTarget->user_sub_project_id = $userSubProject->id;
