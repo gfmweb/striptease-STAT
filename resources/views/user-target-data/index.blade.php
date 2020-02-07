8257e2063187227a3099e95e251c2064db1305ac
Anton <banderazz@inbox.ru>
2020-02-07 14:04:49 +0300

    верстка фильтра для данных по таргетам

--- resources/views/user-target-data/index.blade.php
+++ resources/views/user-target-data/index.blade.php
@@ -5,71 +5,75 @@
 		<div class="col-lg-12">
 			<div class="card">
 				<div class="card-body">
-					<div class="card-title">Отчет по партнерам</div>
+					<div class="card-title mb-4">Отчет по партнерам</div>
 					{{--VUE--}}
 					<div id="vue-report-query-builder" class="data-block">
 						<loading-block :loading="loading"></loading-block>
 						<div class="data-filter">
-							<form class="data-filter-form m-3 form-inline">
-								<div class="form-group m-1">
-									<label for="subProject">Город</label><br>
-									<select name="city" id="city" class="custom-select"
-											v-model="selectedCityId">
-										<option value="all" v-if="hasElements(cities.list)">Все</option>
-										<option v-for="(city,id) of cities.list" :value="id">
-											@{{ city }}
-										</option>
-									</select>
-								</div>
-								<div class="form-group m-1">
-									<label for="subProject">Проект</label><br>
-									<select name="subProject" id="subProject" class="custom-select"
-											v-model="selectedSubProjectId">
-										<option value="all" v-if="hasElements(subProjects.list)">Все</option>
-										<option v-for="(subProject,id) of subProjects.list" :value="id">
-											@{{ subProject }}
-										</option>
-									</select>
-								</div>
-								<div class="form-group m-1">
-									<label for="subProject">Исполнитель</label><br>
-									<select name="partner" id="partner" class="custom-select"
-											v-model="selectedPartnerId">
-										<option value="all" v-if="hasElements(partners.list)">Все</option>
-										<option v-for="(partner,id) of partners.list" :value="id">
-											@{{partner }}
-										</option>
-									</select>
-								</div>
-								<div class="form-group m-1">
-									<label for="subProject">Канал</label><br>
-									<select name="channel" id="channel" class="custom-select"
-											v-model="channels.selectedId">
-										<option value="all" v-if="hasElements(channels.list)">Все</option>
-										<option v-for="(channel,id) of channels.list" :value="id">
-											@{{channel }}
-										</option>
-									</select>
-								</div>
-								<br>
-								<div class="form-group m-1">
-									<label for="date-range" class="mr-3">Период:</label>
-									<div class="input-group input-daterange" name="date-range" id="date-range">
-										<input type="text" class="custom-select datepicker-input" name="dateFrom" :value="dateFrom" @input="changeDates($emit)">
-										<div class="mx-2 mt-2">по</div>
-										<input type="text" class="custom-select datepicker-input" name="dateTo" :value="dateTo">
+							<form class="data-filter-form">
+								<div class="form-row">
+									<div class="form-group col-md-3">
+										<label for="subProject">Город</label>
+										<select name="city" id="city" class="form-control form-control-sm"
+												v-model="selectedCityId">
+											<option value="all" v-if="hasElements(cities.list)">Все</option>
+											<option v-for="(city,id) of cities.list" :value="id">
+												@{{ city }}
+											</option>
+										</select>
 									</div>
-									{{--<input type="text" class="custom-select" name="date-range"  id="date-range" value=""
-										   placeholder="Период"
-										   readonly/>--}}
+									<div class="form-group col-md-3">
+										<label for="subProject">Проект</label>
+										<select name="subProject" id="subProject" class="form-control form-control-sm"
+												v-model="selectedSubProjectId">
+											<option value="all" v-if="hasElements(subProjects.list)">Все</option>
+											<option v-for="(subProject,id) of subProjects.list" :value="id">
+												@{{ subProject }}
+											</option>
+										</select>
+									</div>
+									<div class="form-group col-md-3">
+										<label for="subProject">Исполнитель</label>
+										<select name="partner" id="partner" class="form-control form-control-sm"
+												v-model="selectedPartnerId">
+											<option value="all" v-if="hasElements(partners.list)">Все</option>
+											<option v-for="(partner,id) of partners.list" :value="id">
+												@{{partner }}
+											</option>
+										</select>
+									</div>
+									<div class="form-group col-md-3">
+										<label for="subProject">Канал</label>
+										<select name="channel" id="channel" class="form-control form-control-sm"
+												v-model="channels.selectedId">
+											<option value="all" v-if="hasElements(channels.list)">Все</option>
+											<option v-for="(channel,id) of channels.list" :value="id">
+												@{{channel }}
+											</option>
+										</select>
+									</div>
 								</div>
-								<div class="form-group m-1">
-									<div class="btn btn-success" v-if="filterSettled" @click="loadReport()">
-										Показать
+
+								<div class="form-row">
+									<div class="form-group col-md-6">
+										<div class="input-group input-daterange" name="date-range" id="date-range">
+											<div class="mx-2 mt-2">Период с </div>
+											<input type="text" class="form-control form-control-sm datepicker-input" name="dateFrom" :value="dateFrom" @input="changeDates($emit)">
+											<div class="mx-2 mt-2">по</div>
+											<input type="text" class="form-control form-control-sm datepicker-input" name="dateTo" :value="dateTo">
+										</div>
+										{{--<input type="text" class="custom-select" name="date-range"  id="date-range" value="" placeholder="Период" readonly/>--}}
+									</div>
+									<div class="form-group col-md-3">
+										<div class="btn btn-success" v-if="filterSettled" @click="loadReport()">
+											Показать
+										</div>
 									</div>
 								</div>
 							</form>
 						</div>
+
+						<hr>
 
 						<div class="data-table" v-if="loaded" v-html="report"></div>
 					</div>
