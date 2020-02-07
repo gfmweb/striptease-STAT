239f176bec90ba35ad7722e3c83ff361ec57fb62
Anton <banderazz@inbox.ru>
2020-02-07 17:25:58 +0300

    дополнительные емайлы для пользователей

--- resources/views/partners/partials/form.blade.php
+++ resources/views/partners/partials/form.blade.php
@@ -5,21 +5,31 @@
 	</div>
 </div>
 <div class="form-group row">
-	{!! Form::label('email', 'E-mail', ['class' => 'col-sm-2 col-form-label']) !!}
+	{!! Form::label('email', 'Основной e-mail', ['class' => 'col-sm-2 col-form-label']) !!}
 	<div class="col-sm-10">
-		{!! Form::text('email',null, ['class' => 'form-control', 'required' => "true"] )!!}
+		{!! Form::email('email',null, ['class' => 'form-control', 'required' => "true"] )!!}
+		<span class="help-block">Основной e-mail</span>
+	</div>
+</div>
+<div class="form-group row">
+	{!! Form::label('email_add', 'Доп. e-mail', ['class' => 'col-sm-2 col-form-label']) !!}
+	<div class="col-sm-10">
+		{!! Form::text('email_add',null, ['class' => 'form-control', 'required' => "true"] )!!}
+		<span class="help-block">Дополнительные e-mail'ы, указываются через запятую</span>
 	</div>
 </div>
 <div class="form-group row">
 	{!! Form::label('login', 'Логин', ['class' => 'col-sm-2 col-form-label']) !!}
 	<div class="col-sm-10">
 		{!! Form::text('login',null, ['class' => 'form-control', 'required' => "true"] )!!}
+		<span class="help-block">Логин пользователя в системе статистики</span>
 	</div>
 </div>
 <div class="form-group row">
 	{!! Form::label('password', 'Пароль', ['class' => 'col-sm-2 col-form-label']) !!}
 	<div class="col-sm-10">
 		{!! Form::text('password',null, ['class' => 'form-control', 'required' => "true"] )!!}
+		<span class="help-block">Пароль пользователя в системе статистики</span>
 	</div>
 </div>
 <div class="form-group row">
