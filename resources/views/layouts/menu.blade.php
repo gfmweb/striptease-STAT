42f914c1d747c09c1b504fe7852fefbdb680bc64
Anton <banderazz@inbox.ru>
2020-02-11 12:43:56 +0300

    счетчит "моих проектов"

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -5,7 +5,12 @@
 	</li>
 	<li class="nav-label">Проекты</li>
 	<li>
-		<a href="{{ route('user-targets') }}"><i class="mdi mdi-view-dashboard"></i> <span class="nav-text">Мои проекты</span></a>
+		<a @if (Auth::user()->subProjects->count()) href="{{ route('user-targets') }}" @endif>
+
+			<i class="mdi mdi-view-dashboard"></i>
+			<span class="nav-text">Мои проекты</span>
+			<span class="badge badge-primary nav-badge">{{ Auth::user()->subProjects->count() }}</span>
+		</a>
 	</li>
 	@if (Auth::user()->isAdmin() || Auth::user()->isSuperAdmin())
 		<li>
