e206a6c184e00991e166ba1e590686545e6b84dd
Vargo <to@7ever.ru>
2020-09-10 13:10:09 +0300

    Обновил меню админа и пару правок по части стиля таблиц отчетов

--- resources/views/layouts/menu/admin.blade.php
+++ resources/views/layouts/menu/admin.blade.php
@@ -1,8 +1,7 @@
-<li class="nav-label">Внесение данных</li>
+<li class="nav-label">Данные</li>
 
 <li><a href="{{ route('user-target-data.create') }}"><i class="mdi mdi-table-edit"></i> <span class="nav-text">Данные по проектам</span></a></li>
 <li><a href="{{ route('password-city-data.create') }}"><i class="mdi mdi-table-edit"></i> <span class="nav-text">Приходы по паролям</span></a></li>
-<li><a href="{{ route('digest-data.create') }}"><i class="mdi mdi-table-edit"></i> <span class="nav-text">Дайджест заполнение</span></a></li>
 
 <li class="nav-label">Проекты</li>
 
@@ -21,6 +20,7 @@
 <li><a href="{{ route('reports.my') }}"><i class="ion-android-list"></i> <span class="nav-text">Отчет по партнерам(личный)</span></a></li>
 <li><a href="{{ route('reports.passwords') }}"><i class="ion-android-list"></i><span class="nav-text">Отчет по паролям</span></a></li>
 <li><a href="{{ route('reports.digest') }}"><i class="ion-android-list"></i><span class="nav-text">Отчет по дайджесту</span></a></li>
+<li><a href="{{ route('reports.channels') }}"><i class="ion-android-list"></i><span class="nav-text">Статусы каналов</span></a></li>
 
 <li class="nav-label">Справочники</li>
 
